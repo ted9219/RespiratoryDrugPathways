e8c66608d590e1ca97a8253bd23d71bae1230194
Peter Rijnbeek <p.rijnbeek@erasmusmc.nl>
2020-09-22 11:10:34 -0400

    update cohort counts sql server

--- inst/SQL/CohortCounts.sql
+++ inst/SQL/CohortCounts.sql
@@ -1,8 +1,6 @@
 
-USE @resultsSchema;
-
 SELECT cohort_definition_id,
 	COUNT(*) AS cohort_count,
 	COUNT(DISTINCT subject_id) AS person_count
-FROM @cohortTable
+FROM @resultsSchema.@cohortTable
 GROUP BY cohort_definition_id;
