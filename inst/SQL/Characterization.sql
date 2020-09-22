2fd76dd172ff540aad34412abc12ea16085934e8
Peter Rijnbeek <p.rijnbeek@erasmusmc.nl>
2020-09-22 11:11:08 -0400

    adjust for sql server (start)

--- inst/SQL/Characterization.sql
+++ inst/SQL/Characterization.sql
@@ -1,34 +1,33 @@
-USE @resultsSchema;
 
-IF OBJECT_ID('@studyName_targetcohort', 'U') IS NOT NULL
-DROP TABLE @studyName_targetcohort;
+IF OBJECT_ID('@resultsSchema.@studyName_targetcohort', 'U') IS NOT NULL
+DROP TABLE @resultsSchema.@studyName_targetcohort;
 
-IF OBJECT_ID('@studyName_characterization', 'U') IS NOT NULL
-DROP TABLE @studyName_characterization;
+IF OBJECT_ID('@resultsSchema.@studyName_characterization', 'U') IS NOT NULL
+DROP TABLE @resultsSchema.@studyName_characterization;
 
 -- Load target population into targetcohort table
-CREATE TABLE @studyName_targetcohort
+CREATE TABLE @resultsSchema.@studyName_targetcohort
 (
-PERSON_ID BIGINT NOT NULL PRIMARY KEY,
+PERSON_ID BIGINT NOT NULL,
 INDEX_DATE date NOT NULL,
 COHORT_END_DATE date NOT NULL
 );
 
-INSERT INTO @studyName_targetcohort (PERSON_ID, INDEX_DATE, COHORT_END_DATE)
+INSERT INTO @resultsSchema.@studyName_targetcohort (PERSON_ID, INDEX_DATE, COHORT_END_DATE)
 SELECT
   c.subject_id,
   -- subject_id is equal to person_id
   c.cohort_start_date,
   -- cohort_start_date is equal to index_date
   c.cohort_end_date -- cohort_end_date is equal to cohort_end_date
-FROM @cohortTable C
+FROM @resultsSchema.@cohortTable C
 WHERE C.cohort_definition_id = @targetCohortId;
 
 -- Do characterization
-CREATE TABLE @studyName_characterization AS
+CREATE TABLE @resultsSchema.@studyName_characterization AS
 select gender, count(*) as num_people, avg(age) as avg_age, avg(time_in_cohort) as avg_time_in_cohort
 from (select gender_source_value as gender, date_part('year', age (p.birth_datetime::date)) as age, date_part('day', t.cohort_end_date :: TIMESTAMP - t.index_date :: TIMESTAMP) AS time_in_cohort
-FROM @studyName_targetcohort as t
+FROM @resultsSchema.@studyName_targetcohort as t
 LEFT JOIN @cdmDatabaseSchema.person as p
 ON t.person_id = p.person_id) as o
 GROUP BY ROLLUP (gender);
